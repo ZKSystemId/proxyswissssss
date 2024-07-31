d161baaf624039778c3ea3cc519fd31d1b79bd9f
Zigs Kay <zksystemid@gmail.com>
2024-07-31 20:00:50 +0000

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xBBDc92404D1ad59bcd505cbE3d1F8b1Ead785866'
+export const ProxyAdmin = '0xE4C28360f5F8f380e4f596b8209B9B175de6Bdf6'
+export const SWTRImplementation = '0xfA48226C42BF8Fdc79b01586F44e5111B5f5745A'
+    
